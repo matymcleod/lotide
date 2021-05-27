03df06b1c3fe8c06f6f18eb0695bd0f873fd08ae
Mathew <matymcleod@gmail.com>
2021-05-27 15:47:16 +0000

    testing running

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
 
-const string = "hello world";
+// Still need to remove spaces!
+const string = "hello";
 
 const countLetters = function(string) {
   const obj = {};
 
   for (let i = 0; i < string.length; i++) {
     const ch = string[i];
+    // Not 100% sure how this if statement works
     if (!obj[ch]) {
       obj[ch] = 0;
     }
@@ -15,4 +24,8 @@ const countLetters = function(string) {
 };
 
 console.log(countLetters(string));
+
+// Need to learn how to properly run this test! 
+// keep getting: Assertion Failed: [object Object] !=== undefined
+assertEqual(countLetters(string), ({ h: 1, e: 1, l: 2, o: 1 }))
 
