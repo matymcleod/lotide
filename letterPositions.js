8e8fa70353b6371451f2b2982c5e85ee7db74f1a
Mathew <matymcleod@gmail.com>
2021-05-27 22:17:07 +0000

    completion of letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function(actual, expected) {
+  if(actual.length !== expected.length){
+    return false;
+  }
+  for(let i = 0; i < actual.length; i++) {
+    if(actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(actual, expected){
+  if(eqArrays(actual, expected)){
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
+}
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for(let i = 0; i < sentence.length; i++){
+    const letter = sentence[i];
+    if(letter !== " "){
+      if(!Array.isArray(results[letter])){
+      results[letter] = [i];
+    } else {
+      results[letter].push(i);
+    }
+    //results[letter].push(i);
+    console.log(i);
+  }
+}
+  console.log(results);
+  return results;
+};
+letterPositions('hel lo');
+assertArraysEqual(letterPositions("hello").o, [4]);
