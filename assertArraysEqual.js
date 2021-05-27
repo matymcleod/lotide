b51b0605347e7c3122013a00229293ea430e28bf
Mathew <matymcleod@gmail.com>
2021-05-27 22:18:42 +0000

    fixed up assertArraysEquals.js few mistakes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,24 @@
 // this function compares two arrays and returns a Boolean value that indicates whether they are equal or not.
 // ie: eqArrays([1], [1]) will return true
 // ie: eqArrays([1], [2]) will return false
-const eqArrays = function(arr1, arr2) {
-  let isEqual = true;
-  for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
-      isEqual = false;
-      break;
+const eqArrays = function(actual, expected) {
+  if(actual.length !== expected.length){
+    return false;
+  }
+  for(let i = 0; i < actual.length; i++) {
+    if(actual[i] !== expected[i]) {
+      return false;
     }
   }
-  return isEqual;
+  return true;
 }
 
 // assertArraysEqual compares two arrays and prints a Boolean value to the console indicating whether the two arrays are equal.
-const assertArraysEqual = function(arr1, arr2){
-  console.log(eqArrays(arr1, arr2));
+const assertArraysEqual = function(actual, expected){
+  if(eqArrays(actual, expected)){
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
 }
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2], [1, 2, 3]);
