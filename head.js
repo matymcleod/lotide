b43a4461dc4d467df86d124ab77e1efc9a57abe5
Mathew <matymcleod@gmail.com>
2021-06-02 23:19:18 +0000

    adding head.js to commit

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-const assertEqual = require('./assertEqual');
 
 const head = function(numbers){
   return numbers[0];
@@ -7,6 +6,3 @@ const head = function(numbers){
 
 module.exports = head;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
