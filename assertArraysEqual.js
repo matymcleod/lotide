285958dd37f4792eaf27da700a8c88f5f273e5c7
Mathew <matymcleod@gmail.com>
2021-06-23 01:56:49 +0000

    refactoring assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,11 +2,14 @@ const eqArrays = require('./eqArrays');
 // assertArraysEqual compares two arrays and prints a Boolean value to the console indicating whether the two arrays are equal.
 const assertArraysEqual = function(actual, expected){
   if(eqArrays(actual, expected)){
-    console.log(`Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion failed: ${actual} !== ${expected}`);
+    console.log(`✅✅✅  Assertion Passed: ${actual} IS EQUAL TO ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑  Assertion Failed: ${actual} IS NOT EQUAL TO ${expected}`);
   }
 }
 // assertArraysEqual([1, 2, 3], [1, 2, 3]);
 // assertArraysEqual([1, 2], [1, 2, 3]);
+// assertArraysEqual(["word", "to", "your", "mother"], ["word", "to", "your", "mother"]);
+// assertArraysEqual(["word", "to", "your", "mother"], ["word", "to", "your", "father"]);
+// assertArraysEqual(["word", "to", "your", "mother"], ["word", "to", "your"]);
 module.exports = assertArraysEqual;
