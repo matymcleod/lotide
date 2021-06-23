b83aae6ecf3210d38789abb7fc03e5adc4070e2a
Mathew <matymcleod@gmail.com>
2021-06-23 01:32:46 +0000

    running tests on eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,10 +8,15 @@ const eqArrays = function(arr1, arr2) {
     }
   }
   return isEqual;
-}
+};
+
+// console.log(eqArrays([1, 2, 3], [1, 2])); // => false);
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true);
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false);
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true);
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false);
 
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 // assertEqual(eqArrays([1, 2], [1, 2, 3]), true);
-// eqArrays([1, 2, 3], [1, 2, 3]);
-// eqArrays([1, 2, 3], [1, 2]);
+
 module.exports = eqArrays;
