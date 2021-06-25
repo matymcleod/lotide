2f9456423f38467910fb45846b4298fed2ef0db9
matymcleod <matymcleod@gmail.com>
2021-06-25 02:51:48 +0000

    refactoring map.js

--- map.js
+++ map.js
@@ -2,31 +2,14 @@
 // this function compares two arrays and returns a Boolean value that indicates whether they are equal or not.
 // ie: eqArrays([1], [1]) will return true
 // ie: eqArrays([1], [2]) will return false -----
-const eqArrays = function(actual, expected) {
-  if(actual.length !== expected.length){
-    return false;
-  }
-  for(let i = 0; i < actual.length; i++) {
-    if(actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays')
 
 // ----- assertArraysEqual ----- 
 // compares two arrays and prints a Boolean value to the console indicating whether the two arrays are equal. -----
-const assertArraysEqual = function(actual, expected){
-  if(eqArrays(actual, expected)){
-    console.log(`Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion failed: ${actual} !== ${expected}`);
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual')
 
 // ----- Map function -----
 // This is a Map function that  has been called by 'const results1' variable. 'const results1 is passing in the 'words' array and an arrow function. The arrow function is then is called within the for loop and passed (i) of the words array as its parameter. The arrow function returns (i)'s [0] index which in this case is the first letter of each string in the array.
-
 
 
 // Is called by results1 and passed the 'words' array for the first parameter then, 'word => word[0]' for the callback parameter.
@@ -37,16 +20,13 @@ const map = function(array, callback) {
     results.push(callback(i)) // (word => word) === (i => i) === (function(i){return i;}) 
   }
   return results;
-}
-//////////////////////////////////////////////////////////////
+};
 const words = ["ground", "control", "to", "major", "tom"];
-
 // results1 CALLS the MAP funciton above and passes in (array === words, callback === (word => word[0]) 
 const results1 = map(words, word => word[0]);
-/////////////////////////////////////////////////////////////
 
 
 // Test code:
-assertArraysEqual(words, map(words, word => word));
-assertArraysEqual(words, map(words, word => word[0]));
-console.log(map(words, x => x));
+assertArraysEqual(words, map(words, word => word)); // => pass
+assertArraysEqual(words, map(words, word => word[0])); // => fail
+console.log(map(words, x => x)); // logging the result of the map function
