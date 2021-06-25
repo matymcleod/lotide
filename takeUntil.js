753f981b1104471f6c6443f06bf304c262f47ff7
matymcleod <matymcleod@gmail.com>
2021-06-25 03:06:54 +0000

    refactoring takeUntil to require eqArrays and assertArraysEqual as requirements

--- takeUntil.js
+++ takeUntil.js
@@ -1,26 +1,10 @@
 // this function compares two arrays and returns a Boolean value that indicates whether they are equal or not.
 // ie: eqArrays([1], [1]) will return true
 // ie: eqArrays([1], [2]) will return false
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 // assertArraysEqual compares two arrays and prints a Boolean value to the console indicating whether the two arrays are equal.
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual')
 
 const takeUntil = function(array, callback) {
   const results = [];
@@ -33,17 +17,15 @@ const takeUntil = function(array, callback) {
 };
 
 const data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-const results1 = takeUntil(data1, x => x > 9);
+const results1 = takeUntil(data1, x => x > 9); 
+// x => x > 9 is the same as: const myFunc = function(x) {if(x < 9){return x;}};
 console.log(results1);
-
-
 
 const data2 = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 const results2 = (takeUntil(data2, x => x === 'monday'));
 console.log(results2);
 
 // TESTS
-
 // Fails
 assertArraysEqual(results1, [9, 10, 11, 12, 13]);
 assertArraysEqual(results2, ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']);
