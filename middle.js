a3dee971402576b8755ac7c8fa6052b344fb78cb
matymcleod <matymcleod@gmail.com>
2021-06-28 23:54:38 +0000

    final refactor of middle, tail and middleTest

--- middle.js
+++ middle.js
@@ -9,15 +9,17 @@ const middle = function(array) {
     output.splice(1, 0, array[middle + 1]);
   } else if (array.length > 2) {
     output.splice(0, 0, array[middle + 0.5]);
+  } else if (array.length <= 1) {
+    return [];
   }
   return output;
 };
 
-// console.log('middle with 0 elements: ', middle([]));
-// console.log('middle with 1 elements: ', middle([1]));
+console.log('middle with 0 elements: ', middle([]));
+console.log('middle with 1 elements: ', middle([1]));
 // console.log('middle with 2 elements: ', middle([1, 2]));
 // console.log('middle with 3 elements: ', middle([1, 2, 3]));
-// console.log('middle with 4 elements: ', middle([1, 2, 3, 4]));
+console.log('middle with 4 elements: ', middle([1, 2, 3, 4]));
 // console.log('middle with 5 elements: ', middle([1, 2, 3, 4, 5]));
 
 // assertArraysEqual(middle([1]), []); // => []
--- tail.js
+++ tail.js
@@ -1,7 +1,7 @@
 // Returns all elements of an array except for the first element at index 0.
 
 const tail = function(array) {
-  let end = array.slice(1, array.length);
+  let end = array.slice(-1, array.length);
   return end;
 };
 
